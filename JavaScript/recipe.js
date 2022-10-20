3031ade656d24fe732b2b9e84d767a8c2741e4d6
boazzzzz <boaz0105@gmail.com>
2022-10-20 14:19:08 +0800

    modify all

---
+++ JavaScript/recipe.js
@@ -0,0 +1,49 @@
+var swiper = new Swiper(".slide-content", {
+   slidesPerView: 3,
+   spaceBetween: 25,
+   loop: true,
+   loopFillGroupWithBlank: true,
+   fade: true,
+   centerSlide: true,
+   pagination: {
+     el: ".swiper-pagination",
+     clickable: true,
+     dynamicBullets: true
+   },
+
+   navigation: {
+     nextEl: ".swiper-button-next",
+     prevEl: ".swiper-button-prev",
+   },
+
+   breakpoints: {
+      0: {
+         slidesPerView: 1,
+      },
+      
+      428: {
+         slidesPerView: 2,
+      },
+      
+      1200: {
+         slidesPerView: 3,
+      },
+   }
+});
+
+// meal feature texts
+
+const featureTexts = document.querySelectorAll(".meal-feature-text");
+
+featureTexts.forEach(text => {
+   text.addEventListener("click", () => {
+      removeActive();
+      text.classList.add("meal-feature-text-active");
+   })
+})
+
+function removeActive() {
+   featureTexts.forEach(text => {
+      text.classList.remove("meal-feature-text-active");
+   })
+}
