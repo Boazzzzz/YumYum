8f2e434ee37dfe97617fbd0d9594e70dd795c9ef
boazzzzz <boaz0105@gmail.com>
2022-10-26 12:40:54 +0800

    fix plan selected

--- JavaScript/plan-selected.js
+++ JavaScript/plan-selected.js
@@ -1,4 +1,7 @@
 function init() {
+
+   // PROGRAM PRICE 
+
    const planCheckbox = document.querySelectorAll('#plan-checkbox');
    const planPrice = document.querySelector('.plan-price');
 
@@ -9,6 +12,40 @@ function init() {
    planCheckbox[1].addEventListener('click', () => {
       planPrice.innerHTML = '<p><strong>$</strong>538</p>';
    })
+
+   // VERTICAL SLIDER
+
+   const sliderContainer = document.querySelector('.slider-container')
+   const slideRight = document.querySelector('.right-slide')
+   const slideLeft = document.querySelector('.left-slide')
+   const upButton = document.querySelector('.up-button')
+   const downButton = document.querySelector('.down-button')
+   const slidesLength = slideRight.querySelectorAll('div').length
+
+let activeSlideIndex = 0
+
+   slideLeft.style.top = `-${(slidesLength - 1) * 100}vh`
+
+   upButton.addEventListener('click', () => changeSlide('up'))
+   downButton.addEventListener('click', () => changeSlide('down'))
+
+   const changeSlide = (direction) => {
+      const sliderHeight = sliderContainer.clientHeight
+      if(direction === 'up') {
+         activeSlideIndex++
+         if(activeSlideIndex > slidesLength - 1) {
+               activeSlideIndex = 0
+         }
+      } else if(direction === 'down') {
+         activeSlideIndex--
+         if(activeSlideIndex < 0) {
+               activeSlideIndex = slidesLength - 1
+         }
+      }
+
+      slideRight.style.transform = `translateY(-${activeSlideIndex * sliderHeight}px)`
+      slideLeft.style.transform = `translateY(${activeSlideIndex * sliderHeight}px)`
+   }
 }
 
 window.addEventListener('load', init, false);
